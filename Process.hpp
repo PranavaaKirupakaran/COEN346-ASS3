adbf22267353ab96373d27179b75b2d46e957b08
r-vaidya <78240414+r-vaidya@users.noreply.github.com>
2022-03-09 19:53:00 -0500

    Create Process.hpp

---
+++ Process.hpp
@@ -0,0 +1,13 @@
+//
+//  Process.hpp
+//  COEN346Scheduler
+//
+//  Created by Rohit Vaidya on 2022-03-09.
+//
+
+#ifndef Process_hpp
+#define Process_hpp
+
+#include <stdio.h>
+
+#endif /* Process_hpp */
