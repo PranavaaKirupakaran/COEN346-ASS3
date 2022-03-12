4a868eb317a093b66bc6ab502980d47a87a50196
PranavaaKirupakaran <pranavaa.kirupakaran@gmail.com>
2022-03-12 16:22:14 -0500

    Update Process.cpp
    made function defintions

--- Process.cpp
+++ Process.cpp
@@ -5,59 +5,70 @@
 //  Created by Rohit Vaidya on 2022-03-09.
 //
 
-#include "Process.hpp"
+#include "Process.h"
 #include <iostream>
 #include <stdio.h>
+#include <string>
+
 using namespace std;
 
-class Process{
-private:
-    string process_id;
-    int arrival_time,burst_time, priority;
-    
-public:
-    Process(string name, int arrive, int burst, int pri)
-    {
-        process_id = name;
-        arrival_time = arrive;
-        burst_time = burst;
-        priority = pri;
-    }
-    
-    void processID(string name)
-    {
-        process_id = name;
-    }
-    
-    void addArrivalTime(int wait)
-    {
-        arrival_time = wait;
-    }
-    
-    void addBurstTime(int burst)
-    {
-        burst_time = burst;
-    }
-    
-    void addPriority(int time)
-    {
-        priority = time;
-    }
-    
-    int getArrivalTime(int wait)
-    {
-        return arrival_time;
-    }
-    
-    int getBurstTime(int burst)
-    {
-        return burst_time;
-    }
-    
-    int getPriority(int time)
-    {
-        return priority;
-    }
-    
-    
+Process::Process(){
+    process_id = "";
+    arrival_time, burst_time, priority, waiting_time, cpu_iteration = 0;
+};
+
+Process::Process(std::string id, int arrivalTime, int burstTime, int p){
+    process_id = id;
+    arrival_time = arrivalTime;
+    burst_time = burstTime;
+    priority = p;
+    waiting_time, cpu_iteration = 0;
+};
+
+void Process::setProcessID(std::string processID){
+    process_id = processID;
+};
+
+std::string Process::getProcessID(){
+    return process_id;
+};
+
+void Process::setArrivalTime(int arrivalTime){
+    arrival_time = arrivalTime;
+};
+
+int Process::getArrivalTime(){
+    return arrival_time;
+};
+
+void Process::setBurstTime(int burstTime){
+    burst_time = burstTime;
+};
+
+int Process::getBurstTime(){
+    return burst_time;
+};
+
+void Process::setPriority(int p){
+    priority = p;
+};
+
+int Process::getPriority(){
+    return priority;
+};
+
+void Process::setWaitingTime(int waitingTime){
+    waiting_time = waitingTime;
+};
+
+int Process::getWaitingTime(){
+    return waiting_time;
+};
+
+void Process::setCpuIteration(int iteration){
+    cpu_iteration = iteration;
+};
+
+int Process::getCpuIteration(){
+    return cpu_iteration;
 };
