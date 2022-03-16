16a2a12d87fe9bb468a6afc87136090b4df43b19
Pranavaa-Kirupakaran <43556499+PranavaaKirupakaran@users.noreply.github.com>
2022-03-15 23:36:33 -0400

    added latest version

---
+++ ProcessQueue.cpp
@@ -0,0 +1,106 @@
+//
+//  ProcessQueue.cpp
+//  COEN346Scheduler
+//
+//  Created by Rohit Vaidya on 2022-03-12.
+//
+#include "ProcessQueue.h"
+
+using namespace std;
+
+ProcessQueue::ProcessQueue() {
+    flag = true;
+}
+
+ProcessQueue::ProcessQueue(bool active) {
+    flag = active;
+}
+
+ProcessQueue::~ProcessQueue() {
+
+}
+
+void ProcessQueue::addProcess(Process* p) {
+    plist.push(p);
+}
+
+void ProcessQueue::updateFlag() {
+    flag = !flag;
+}
+
+bool ProcessQueue::getFlag() {
+    return flag;
+}
+
+Process* ProcessQueue::removeProcess() {
+    Process* temp = plist.front();
+    plist.pop();
+    return temp;
+}
+
+int ProcessQueue::minIndex(int sortedIndex)
+{
+    int min_index = -1;
+    int min_val = 140;
+    int n = plist.size();
+    for (int i = 0; i < n; i++)
+    {
+        Process* curr = plist.front();
+        plist.pop();  // This is dequeue() in C++ STL
+
+        // we add the condition i <= sortedIndex
+        // because we don't want to traverse
+        // on the sorted part of the queue,
+        // which is the right part.
+        if (curr->getPriority() <= min_val && i <= sortedIndex)
+        {
+            min_index = i;
+            min_val = curr->getPriority();
+        }
+        plist.push(curr);  // This is enqueue() in
+                       // C++ STL
+    }
+    return min_index;
+}
+
+// Moves given minimum element to rear of
+// queue
+void ProcessQueue::insertMinToRear(int min_index)
+{
+    Process* min_val;
+    int n = plist.size();
+    for (int i = 0; i < n; i++)
+    {
+        Process* curr = plist.front();
+        plist.pop();
+        if (i != min_index)
+            plist.push(curr);
+        else
+            min_val = curr;
+    }
+    plist.push(min_val);
+}
+
+void ProcessQueue::sort()
+{
+    for (int i = 1; i <= plist.size(); i++)
+    {
+        int min_index = minIndex(plist.size() - i);
+        insertMinToRear(min_index);
+    }
+}
+
+void ProcessQueue::printQueue() {
+    Process* temp;
+    queue<Process*> tempList = plist;
+    while (!tempList.empty()) {
+        temp = tempList.front();
+        tempList.pop();
+        cout << "PID: " << temp->getProcessID() << " Priority: " << temp->getPriority() << endl;
+    }
+}
+
+bool ProcessQueue::checkEmpty() {
+    return plist.empty();
+}
+
---
+++ Scheduler.h
@@ -0,0 +1,45 @@
+#pragma once
+//
+//  Scheduler.hpp
+//  COEN346Scheduler
+//
+//  Created by Rohit Vaidya on 2022-03-13.
+//
+
+#ifndef Scheduler_h
+#define Scheduler_h
+
+#include <stdio.h>
+#include <string>
+//#include "Clock.cpp"
+#include "ProcessQueue.h"
+#include <thread>
+
+class Scheduler {
+
+private:
+    Clock* clk;
+    ProcessQueue q1;
+    ProcessQueue q2;
+    int timeSlice;
+    vector<thread> threadVector;
+    bool terminated;
+
+public:
+    Scheduler();
+    int calculateTimeSlice(Process* p);
+    int calculatePriority(Process* p);
+    void swapFlag();
+    void addProcess(Process* p);
+    void schedule();
+    void setClock(Clock* c);
+    ProcessQueue* getActiveQueue();
+    ProcessQueue* getExpiredQueue();
+    void sleepScheduler();
+    void setTerminated(bool flag);
+    bool getTerminated();
+
+};
+#endif /* Scheduler_hpp */
+
+
