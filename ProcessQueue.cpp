99380bc22f7043e71f81e52c98bdc565da65ef00
Pranavaa-Kirupakaran <43556499+PranavaaKirupakaran@users.noreply.github.com>
2022-03-30 19:35:14 -0400

    reboot systems
    backup loaded

---
+++ ProcessQueue.cpp
@@ -0,0 +1,107 @@
+//
+//  ProcessQueue.cpp
+//  COEN346Scheduler
+//
+//  Created by Rohit Vaidya on 2022-03-12.
+//
+#include "ProcessQueue.h"
+
+using namespace std;
+
+//Set queue as active upon initialization
+ProcessQueue::ProcessQueue() {
+    flag = true;
+}
+
+ProcessQueue::ProcessQueue(bool active) {
+    flag = active;
+}
+
+ProcessQueue::~ProcessQueue() {
+
+}
+
+void ProcessQueue::addProcess(Process* p) {
+    plist.push(p);
+}
+
+//change flag of queue
+void ProcessQueue::updateFlag() {
+    flag = !flag;
+}
+
+bool ProcessQueue::getFlag() {
+    return flag;
+}
+
+//Return a pointer to a process
+Process* ProcessQueue::removeProcess() {
+    Process* temp = plist.front();
+    plist.pop();
+    return temp;
+}
+
+int ProcessQueue::minIndex(int sortedIndex)
+{
+    int min_index = -1;
+    int min_val = 140;
+    int n = plist.size();
+    for (int i = 0; i < n; i++)
+    {
+        Process* curr = plist.front();
+        plist.pop();  // This is dequeue() in C++ STL
+
+        // we add the condition i <= sortedIndex
+        // because we don't want to traverse
+        // on the sorted part of the queue,
+        // which is the right part.
+        if (curr->getPriority() <= min_val && i <= sortedIndex)
+        {
+            min_index = i;
+            min_val = curr->getPriority();
+        }
+        plist.push(curr);  
+    }
+    return min_index;
+}
+
+// Moves given minimum element to rear of queue
+void ProcessQueue::insertMinToRear(int min_index)
+{
+    Process* min_val;
+    int n = plist.size();
+    for (int i = 0; i < n; i++)
+    {
+        Process* curr = plist.front();
+        plist.pop();
+        if (i != min_index)
+            plist.push(curr);
+        else
+            min_val = curr;
+    }
+    plist.push(min_val);
+}
+
+//Sort queue based on priority
+void ProcessQueue::sort()
+{
+    for (int i = 1; i <= plist.size(); i++)
+    {
+        int min_index = minIndex(plist.size() - i);
+        insertMinToRear(min_index);
+    }
+}
+
+void ProcessQueue::printQueue() {
+    Process* temp;
+    queue<Process*> tempList = plist;
+    while (!tempList.empty()) {
+        temp = tempList.front();
+        tempList.pop();
+        cout << "PID: " << temp->getProcessID() << " Priority: " << temp->getPriority() << endl;
+    }
+}
+
+bool ProcessQueue::checkEmpty() {
+    return plist.empty();
+}
