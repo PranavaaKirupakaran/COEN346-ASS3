84db2d2900406d6629c9757e9f77634d32ae1fa2
George Haddad <hd_george@hotmail.com>
2022-04-06 17:11:38 -0400

    Update Command.cpp

--- Command.cpp
+++ Command.cpp
@@ -1,1 +1,39 @@
 #include "Command.h"
+
+using namespace std;
+
+Command::Command(){
+    commandName = "";
+    variableID = "";
+    variableValue = NULL;
+}
+
+Command::Command(string name, string id, int value){
+    commandName = name;
+    variableID = id;
+    variableValue = value;
+}
+
+void Command::setCommandName(string name){
+    commandName = name;
+}
+
+string Command::getCommandName(){
+    return commandName;
+}
+
+void Command::setVariableID(string id){
+    variableID = id;
+}
+
+string Command::getVariableID(){
+    return variableID;
+}
+
+void Command::setVariableValue(int value){
+    variableValue = value;
+}
+
+int Command::getVariableValue(){
+    return variableValue;
+}
